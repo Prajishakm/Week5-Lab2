e5a3f01ac873f4c25569a5be96790e448293b4fc
Prajisha <Prajishakm98@gmail.com>
2025-02-11 14:50:30 +0530

    Lab2 has been updated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import Hello from './hello';
+import Bye from './bye';
+
+// App.jsx
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
